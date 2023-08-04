a34d28f756d135e06b78523c22d0caea7e94c643
Kz12138 <804411280@qq.com>
2023-08-04 15:58:31 +0800

    加上注释

--- src/wsControl/checkUser.js
+++ src/wsControl/checkUser.js
@@ -6,51 +6,63 @@ import config from "../config";
 import code from "../common/code";
 import { onlineUsers } from "../users";
 
+const isOnline = (user) => {
+    let onlineUser = onlineUsers.find((item) => item.id === user.id);
+    if (onlineUser) {
+        onlineUser.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.kick, "您的账号在其他地方登录，您已被迫下线")));
+        onlineUser.close();
+        onlineUsers.splice(onlineUsers.indexOf(onlineUser), 1);
+    }
+}
+
 //用户token验证
 export default (wss) => {
     wss.eventListeners.on(code.checkUser.code, (ws, msg) => {
         const { token, refreshToken } = msg.data;
+        //verify login token
         verifyToken(token, async (err, decoded) => {
+            //login token error
             if (err) {
+                //verify refresh token
                 verifyToken(refreshToken, async (err, _decoded) => {
+                    //refresh token error
                     if (err) {
                         ws.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.fail, "登录失败，token以及刷新token校验失败")));
                         ws.close();
                         return;
                     }
+                    //refresh token success
+                    //get user info
                     let user = await db('tb_user').where({ account: _decoded.account }).first();
+                    //user not exist or refresh token not same or login token not same
                     if (!user || (getToken('refresh' + user.id) !== refreshToken) || getToken('login' + user.id) !== token) {
                         ws.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.fail, "登录失败，用户不存在或刷新token不正确")));
                         ws.close();
                         return;
                     }
-                    let onlineUser = onlineUsers.find((item) => { item.id === user.id });
-                    if (onlineUser) {
-                        onlineUser.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.kick, "您的账号在其他地方登录，您已被迫下线")));
-                        onlineUser.close();
-                        onlineUsers.splice(onlineUsers.indexOf(onlineUser), 1);
-                    }
+                    //judge have user is online
+                    isOnline(user);
+                    //create new token and refresh token
                     let newToken = createToken(user, config.token.login);
                     let newRefreshToken = createToken(user, config.token.refresh);
                     setToken('login' + user.id, newToken);
                     setToken('refresh' + user.id, newRefreshToken);
                     ws.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.success, { token: newToken, refreshToken: newRefreshToken })));
-                    onlineUsers.push(ws);
+                    ws.login = true;
                 });
             }
             else {
+                //login token success
+                //get user info
                 let user = await db('tb_user').where({ account: decoded.account }).first();
+                //user not exist or login token not same
                 if (!user || getToken('login' + user.id) !== token) {
                     ws.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.fail, "登录失败，用户不存在或token不正确")));
                     ws.close();
                     return;
                 }
-                let onlineUser = onlineUsers.find((item) => { item.id === user.id });
-                if (onlineUser) {
-                    onlineUser.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.kick, "您的账号在其他地方登录，您已被迫下线")));
-                    onlineUser.close();
-                    onlineUsers.splice(onlineUsers.indexOf(onlineUser), 1);
-                }
+                //judge have user is online
+                isOnline(user);
                 ws.send(JSON.stringify(new msgBase(code.checkUser.code, code.checkUser.state.success, { userName: user.nickname })));
                 ws.login = true;
                 ws.id = user.id;
